3a32b535027cddf99fca259b7763859b5bc66825
Joe <hutcheson2001@yahoo.com>
2022-02-10 07:25:53 -0500

    commit tree 8fb1b18f455d9425f997208a771f4193838034c0

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+//FIRST ATTEMPT 
+// function isPalindrome(word) {
+//   let wordLetters = word.split("");
+//   let reverseWord = wordLetters.reverse();
+//   let originalWord = wordLetters.join("");
+//   let backwardWord = reverseWord.join("");
+//   return Boolean(originalWord === backwardWord);
+// }
+
+//CLEANED UP VERSION
 function isPalindrome(word) {
-  // Write your algorithm here
+  let backwardWord = word.split("").reverse().join("");
+  return word === backwardWord;
 }
 
 /* 
-  Add your pseudocode here
+  take an argument of a word
+  split the word by letters into an array.
+  add each letter of that array into a new array in revers order.
+  compare to the two arrays.
 */
 
 /*
-  Add written explanation of your solution here
+ Taking the word as an argument, the function first splits the word into letters into an array. Then, we iterate through the area and put the letters in reverse order. Then we rejoin the words and compare them, returning a boolean value. 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  isPalindrome("racecare")
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
